8b573a91dda2d305a3ddf94d70beb915148fd3c9
Jonny Hork <jhork@salesforce.com>
2020-07-28 10:19:19 -0600

    use web component standard api

--- packages/soql-builder-ui/src/index.ts
+++ packages/soql-builder-ui/src/index.ts
@@ -1,9 +1,8 @@
 // @ts-ignore
-import { createElement } from 'lwc';
-// @ts-ignore
 import App from 'qb/app';
 
-const app = createElement('qb-app', { is: App });
+customElements.define('qb-app', App.CustomElementConstructor);
+const app = document.createElement('qb-app');
 // @ts-ignore
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
