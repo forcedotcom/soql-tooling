4786c57f587a2120cdc4599ec91bf59423afb39f
Jonny Hork <jhork@salesforce.com>
2020-07-30 14:29:17 -0600

    populate object selection with mock data

--- packages/soql-builder-ui/src/modules/querybuilder/app/app.ts
+++ packages/soql-builder-ui/src/modules/querybuilder/app/app.ts
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
+import { ToolingService } from '../../../toolingService/toolingService';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+  sObjects: string[];
+
+  connectedCallback() {
+    let toolingService = new ToolingService();
+    this.sObjects = toolingService.sObjects;
+    console.log('sObjects', this.sObjects);
+  }
+}
--- packages/soql-builder-ui/src/modules/querybuilder/from/from.ts
+++ packages/soql-builder-ui/src/modules/querybuilder/from/from.ts
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class From extends LightningElement {}
+export default class From extends LightningElement {
+  @api sobjects;
+}
