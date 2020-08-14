2172b8195cfca047726ce990a0cfbcd8248b2ab3
Jonny Hork <jhork@salesforce.com>
2020-08-14 14:32:53 -0600

    update interfaces

--- packages/soql-builder-ui/src/modules/querybuilder/services/modelService.ts
+++ packages/soql-builder-ui/src/modules/querybuilder/services/modelService.ts
@@ -1,63 +1,93 @@
 /* eslint-disable no-unused-vars */
-import { fromJS, List, Map } from "immutable";
-import { BehaviorSubject, Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { fromJS, List, Map } from 'immutable';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { JsonMap } from '@salesforce/ts-types';
 
-export interface SoqlQueryModel extends Map<string, string | List<string>> {
-    sObject: string;
-    fields: List<string>;
+// This is to satisfy TS and stay dry
+export type IMap = Map<string, string | List<string>>;
+// Private immutable interface
+interface SoqlQueryModel extends IMap {
+  sObject: string;
+  fields: List<string>;
 }
+// Public inteface for accessing modelService.query
+export interface SoqlModelJson extends JsonMap {
+  sObject: string;
+  fields: string[];
+}
+
 export class ModelService {
+  private model: BehaviorSubject<SoqlQueryModel>;
+  public query: Observable<SoqlModelJson>;
+  private defaultQueryModel: IMap;
 
-    private model: BehaviorSubject<SoqlQueryModel>;
-    public query: Observable<SoqlQueryModel>;
-    private defaultQueryModel: Map<string, string | List<string>>;
+  constructor() {
+    this.defaultQueryModel = Map({
+      sObject: '',
+      fields: List<string>([])
+    });
+    this.model = new BehaviorSubject(
+      fromJS(this.read() || this.defaultQueryModel)
+    );
+    this.query = this.model.pipe(
+      map((soqlQueryModel) => (soqlQueryModel as IMap).toJS())
+    );
+  }
 
-    constructor() {
-        this.defaultQueryModel = Map({
-            sObject: '',
-            fields: List<string>([])
-        })
-        this.model = new BehaviorSubject(fromJS(this.read() || this.defaultQueryModel));
-        this.query = this.model.pipe(map(soqlQueryModel => (soqlQueryModel as Map).toJS()))
-    }
-    public getQuery(): Map {
-        return this.model.getValue();
-    }
-    public getFields() {
-        return (this.getQuery().get("fields") as List<string>);
-    }
-    public setSObject(sObject: string) {
-        this.model.next(fromJS(this.defaultQueryModel).set("sObject", sObject));
-    }
+  public getModel(): IMap {
+    return this.model.getValue();
+  }
 
-    public addField(field: string) {
-        this.model.next(this.getQuery().set("fields", this.getFields().toSet().add(field).toList()) as SoqlQueryModel);
-    }
+  private getFields() {
+    return this.getModel().get('fields') as List<string>;
+  }
 
-    public removeField(field: string) {
-        this.model.next(this.getQuery().set("fields", this.getFields().filter( (item) => { return item !== field}) as List<string>) as SoqlQueryModel);
-    }
+  public setSObject(sObject: string) {
+    this.model.next(fromJS(this.defaultQueryModel).set('sObject', sObject));
+  }
 
-    // BELOW HERE IS JUST TEMPORARY SAVE FUNCTIONALITY TO TEST RE_HYDRATIONG THE UI BASED ON AN EXISTING QUERY
-    public save() {
-        localStorage.setItem("soql", JSON.stringify(this.getQuery().toJS()));
-    }
+  public addField(field: string) {
+    this.model.next(
+      this.getModel().set(
+        'fields',
+        this.getFields().toSet().add(field).toList()
+      ) as SoqlQueryModel
+    );
+  }
 
-    public read() {
-        let saved = localStorage.getItem("soql");
-        try {
-            if (saved) {
-                return JSON.parse(saved);
-            }
-        } catch(e) {
-            console.error(e);
-            saved = undefined;
-        }
-        return saved;
-    }
+  public removeField(field: string) {
+    this.model.next(
+      this.getModel().set(
+        'fields',
+        this.getFields().filter((item) => {
+          return item !== field;
+        }) as List<string>
+      ) as SoqlQueryModel
+    );
+  }
 
-    public clear() {
-        localStorage.clear();
+  // BELOW HERE IS JUST TEMPORARY SAVE FUNCTIONALITY TO TEST RE_HYDRATIONG THE UI BASED ON AN EXISTING QUERY
+  public save() {
+    localStorage.setItem('soql', JSON.stringify(this.getModel().toJS()));
+  }
+
+  public read() {
+    let saved;
+    try {
+      if (localStorage) {
+        saved = localStorage.getItem('soql');
+      }
+      if (saved) {
+        return JSON.parse(saved);
+      }
+    } catch (e) {
+      saved = null;
     }
+    return saved;
+  }
+
+  public clear() {
+    localStorage.clear();
+  }
 }
