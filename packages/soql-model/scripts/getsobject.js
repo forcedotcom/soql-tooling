477562decd92e269a63a73b043bfee056a6730fc
Jonathan Gellin <jgellin@salesforce.com>
2020-08-26 15:47:17 -0400

    add complex case to getsobject sample

--- packages/soql-model/scripts/getsobject.js
+++ packages/soql-model/scripts/getsobject.js
@@ -5,9 +5,15 @@
 var model = require('../lib');
 
 var query = 'SELECT field1, field2 FROM object1';
+// var query =
+//   'SELECT field1, field2, field3 alias3, (SELECT fieldA FROM objectA), TYPEOF obj WHEN typeX THEN fieldX ELSE fieldY END FROM object1 ' +
+//   'WHERE field1 = 5 WITH DATA CATEGORY cat__c AT val__c GROUP BY field1 ORDER BY field2 DESC NULLS LAST LIMIT 20 OFFSET 2 BIND field1 = 5 FOR VIEW UPDATE TRACKING';
+
 var deserializer = new model.ModelDeserializer(query);
 var queryModel = deserializer.deserialize();
-var fields = queryModel.select.selectExpressions.map((expr) => expr.fieldName);
+var fields = queryModel.select.selectExpressions
+  .filter((expr) => !model.SoqlModelUtils.containsUnmodeledSyntax(expr))
+  .map((expr) => expr.fieldName);
 var sObject = queryModel.from.sobjectName;
 console.log(`Query:   ${query}`);
 console.log(`SObject: ${sObject}`);
