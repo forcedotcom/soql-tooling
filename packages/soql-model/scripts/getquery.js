8a368039566dc45510f41c12114e3556516bb6c3
Jonathan Gellin <jgellin@salesforce.com>
2020-09-22 09:48:43 -0400

    further script updates

--- packages/soql-model/scripts/getquery.js
+++ packages/soql-model/scripts/getquery.js
@@ -3,20 +3,26 @@
 // sample script for how to get SOQL script from fields and sobject
 
 var model = require('../lib');
-const { UnmodeledSyntaxImpl, NullsOrder, Order } = require('../lib/model/impl');
 
 var sobjectName = 'object1';
 var fields = ['field1', 'field2'];
-var selectExprs = fields.map((field) => new model.Impl.FieldRefImpl(field));
+var selectExprs = fields.map(
+  (field) =>
+    new model.Impl.FieldSelectionImpl(new model.Impl.FieldRefImpl(field))
+);
 var queryModel = new model.Impl.QueryImpl(
   new model.Impl.SelectExprsImpl(selectExprs),
   new model.Impl.FromImpl(sobjectName),
   new model.Impl.UnmodeledSyntaxImpl('WHERE field1 = 5'),
-  new model.Impl.OrderByExpressionImpl(
-    new model.Impl.FieldRefImpl('shattered'),
-    model.Soql.Order.Ascending,
-    model.Soql.NullsOrder.First
-  )
+  undefined,
+  undefined,
+  new model.Impl.OrderByImpl([
+    new model.Impl.OrderByExpressionImpl(
+      new model.Impl.FieldRefImpl('field1'),
+      model.Soql.Order.Ascending,
+      model.Soql.NullsOrder.First
+    ),
+  ])
 );
 var serializer = new model.ModelSerializer(queryModel);
 var query = serializer.serialize();
