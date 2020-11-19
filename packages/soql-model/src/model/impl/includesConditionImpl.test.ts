65eb027d5d0fb1c9522ba8987c56712e37318dcd
jgellin-sf <55159130+jgellin-sf@users.noreply.github.com>
2020-11-19 12:17:16 -0500

    model WHERE clause (#68)
    * model WHERE clause
    * make IN and INCLUDES conditions unmodeled syntax
    * implement condition simplicity utils
    * added where complexity to script statement

---
+++ packages/soql-model/src/model/impl/includesConditionImpl.test.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import * as Impl from '.';
+import { IncludesOperator, LiteralType } from '../model';
+
+describe('IncludesConditionImpl should', () => {
+  it('store field, operator, and values', () => {
+    const expected = { field: { fieldName: 'field' }, operator: 'INCLUDES', values: [{ type: 'STRING', value: "'abc'" }, { type: 'STRING', value: "'def'" }] };
+    const actual = new Impl.IncludesConditionImpl(
+      new Impl.FieldRefImpl('field'),
+      IncludesOperator.Includes,
+      [new Impl.LiteralImpl(LiteralType.String, "'abc'"), new Impl.LiteralImpl(LiteralType.String, "'def'")]
+    );
+    expect(actual).toEqual(expected);
+  });
+  it('return field, operator, and parenthesized comma-separated values separated by spaces for toSoqlSyntax()', () => {
+    const expected = "field INCLUDES ( 'abc', 'def' )";
+    const actual = new Impl.IncludesConditionImpl(
+      new Impl.FieldRefImpl('field'),
+      IncludesOperator.Includes,
+      [new Impl.LiteralImpl(LiteralType.String, "'abc'"), new Impl.LiteralImpl(LiteralType.String, "'def'")]
+    ).toSoqlSyntax();
+    expect(actual).toEqual(expected);
+  });
+});
--- packages/soql-model/src/model/util.test.ts
+++ packages/soql-model/src/model/util.test.ts
@@ -7,7 +7,19 @@
 
 import { SoqlModelUtils } from './util';
 import * as Impl from './impl';
-import { ErrorType, Query } from './model';
+import * as Soql from './model';
+
+const field = new Impl.FieldRefImpl('field');
+const literal = new Impl.LiteralImpl(Soql.LiteralType.String, "'Hello'");
+const conditionFieldCompare = new Impl.FieldCompareConditionImpl(field, Soql.CompareOperator.EQ, literal);
+const conditionLike = new Impl.LikeConditionImpl(field, literal);
+const conditionInList = new Impl.InListConditionImpl(field, Soql.InOperator.In, [literal]);
+const conditionIncludes = new Impl.IncludesConditionImpl(field, Soql.IncludesOperator.Includes, [literal]);
+const conditionUnmodeled = new Impl.UnmodeledSyntaxImpl('A + B > 10');
+const conditionAndOr = new Impl.AndOrConditionImpl(conditionFieldCompare, Soql.AndOr.And, conditionLike);
+const conditionNested = new Impl.NestedConditionImpl(conditionFieldCompare);
+const conditionNot = new Impl.NotConditionImpl(conditionFieldCompare);
+
 
 describe('SoqlModelUtils should', () => {
   it('return true if SOQL query model contains unmodeled syntax', () => {
@@ -36,22 +48,73 @@ describe('SoqlModelUtils should', () => {
     expect(actual).toBeFalsy();
   });
   it('return true if SOQL query model contains error', () => {
-    const model: Query = new Impl.QueryImpl(
+    const model: Soql.Query = new Impl.QueryImpl(
       new Impl.SelectExprsImpl([new Impl.FieldRefImpl('field1')]),
       new Impl.FromImpl('object1')
     );
-    model.errors = [{ type: ErrorType.UNKNOWN, message: 'ERROR', lineNumber: 1, charInLine: 1 }];
+    model.errors = [{ type: Soql.ErrorType.UNKNOWN, message: 'ERROR', lineNumber: 1, charInLine: 1 }];
 
     const actual = SoqlModelUtils.containsError(model);
     expect(actual).toBeTruthy();
   });
   it('return false if SOQL query model does not contain error', () => {
-    const model: Query = new Impl.QueryImpl(
+    const model: Soql.Query = new Impl.QueryImpl(
       new Impl.SelectExprsImpl([new Impl.FieldSelectionImpl(new Impl.FieldRefImpl('field1'))]),
       new Impl.FromImpl('object1')
     );
 
     const actual = SoqlModelUtils.containsError(model);
     expect(actual).toBeFalsy();
+  });
+  it('return true from isSimpleCondition for simple conditions', () => {
+    const simpleConditions: Soql.Condition[] = [
+      conditionFieldCompare,
+      conditionLike,
+      conditionIncludes,
+      conditionInList,
+      conditionUnmodeled,
+      conditionNested
+    ];
+    let actual = true;
+    simpleConditions.forEach(condition => actual &&= SoqlModelUtils.isSimpleCondition(condition));
+    expect(actual).toBeTruthy();
+  })
+  it('return false from isSimpleCondition for non-simple conditions', () => {
+    const complexConditions: Soql.Condition[] = [
+      conditionAndOr,
+      conditionNot,
+      new Impl.NestedConditionImpl(conditionAndOr)
+    ];
+    let actual = true;
+    complexConditions.forEach(condition => actual &&= !SoqlModelUtils.isSimpleCondition(condition));
+    expect(actual).toBeTruthy();
+  })
+  it('return true from isSimpleGroup for simple group of conditions', () => {
+    const simpleGroups: Soql.Condition[] = [
+      conditionFieldCompare,
+      conditionAndOr,
+      new Impl.AndOrConditionImpl(conditionFieldCompare, Soql.AndOr.And, conditionAndOr),
+      new Impl.NestedConditionImpl(conditionAndOr)
+    ];
+    let actual = true;
+    simpleGroups.forEach(condition => actual &&= SoqlModelUtils.isSimpleGroup(condition));
+    expect(actual).toBeTruthy();
+  });
+  it('return false from isSimpleGroup for non-simple group of conditions', () => {
+    const nonSimpleGroups: Soql.Condition[] = [
+      // NOT
+      conditionNot,
+      // mixing AND and OR
+      new Impl.AndOrConditionImpl(conditionFieldCompare, Soql.AndOr.Or, conditionAndOr),
+      // combined simple groups
+      new Impl.AndOrConditionImpl(
+        new Impl.NestedConditionImpl(conditionAndOr),
+        Soql.AndOr.Or,
+        new Impl.NestedConditionImpl(conditionAndOr)
+      )
+    ];
+    let actual = true;
+    nonSimpleGroups.forEach(condition => actual &&= !SoqlModelUtils.isSimpleGroup(condition));
+    expect(actual).toBeTruthy();
   });
 });
