bfa096fbe2199884c7ca83efe2e57160e3c5857e
Jonny Hork <jhork@salesforce.com>
2020-12-11 09:07:49 -0700

    fix failing test in soqlUtils.ts

--- packages/soql-builder-ui/src/modules/querybuilder/services/soqlUtils.test.ts
+++ packages/soql-builder-ui/src/modules/querybuilder/services/soqlUtils.test.ts
@@ -11,23 +11,49 @@ describe('SoqlUtils', () => {
   const uiModelOne: ToolingModelJson = {
     sObject: 'Account',
     fields: ['Name', 'Id'],
+    where: {
+      conditions: [
+        {
+          field: 'Name',
+          operator: 'EQ',
+          criteria: { type: 'STRING', value: "'pwt'" },
+          index: 0
+        },
+        {
+          field: 'Id',
+          operator: 'EQ',
+          criteria: { type: 'NUMBER', value: '123456' },
+          index: 1
+        }
+      ],
+      andOr: 'AND'
+    },
     orderBy: [{ field: 'Name', order: 'ASC', nulls: 'NULLS FIRST' }],
     limit: '11',
     errors: [],
     unsupported: []
   };
   const soqlOne =
-    'Select Name, Id from Account ORDER BY Name ASC NULLS FIRST LIMIT 11';
+    "Select Name, Id from Account WHERE Name = 'pwt' AND Id = 123456 ORDER BY Name ASC NULLS FIRST LIMIT 11";
   it('transform UI Model to Soql', () => {
     const transformedSoql = convertUiModelToSoql(uiModelOne);
     expect(transformedSoql).toContain(uiModelOne.fields[0]);
     expect(transformedSoql).toContain(uiModelOne.fields[1]);
     expect(transformedSoql).toContain(uiModelOne.sObject);
+    expect(transformedSoql).toContain(
+      uiModelOne.where.conditions[0].criteria.value
+    );
+    expect(transformedSoql).toContain(
+      uiModelOne.where.conditions[1].criteria.value
+    );
+    expect(transformedSoql).toContain(uiModelOne.where.andOr);
+    expect(transformedSoql).toContain('=');
     expect(transformedSoql).toContain(uiModelOne.orderBy[0].field);
     expect(transformedSoql).toContain(uiModelOne.orderBy[0].order);
     expect(transformedSoql).toContain(uiModelOne.orderBy[0].nulls);
     expect(transformedSoql).toContain('11');
   });
+
   it('transforms Soql to UI Model', () => {
     const transformedUiModel = convertSoqlToUiModel(soqlOne);
     expect(transformedUiModel).toEqual(uiModelOne);
