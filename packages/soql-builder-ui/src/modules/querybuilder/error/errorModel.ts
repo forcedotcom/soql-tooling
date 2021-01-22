801ae2e707b32701dbc2e88a22939300de5b4aa8
Dehru Cromer <dehru.cromer@salesforce.com>
2021-01-22 11:43:31 -0700

    use shared error enum

--- packages/soql-builder-ui/src/modules/querybuilder/error/errorModel.ts
+++ packages/soql-builder-ui/src/modules/querybuilder/error/errorModel.ts
@@ -2,38 +2,64 @@
  * ERROR HANDLING UTILITIES
  * THIS CAN BE REPLACED WITH IMPORT FROM SOQL_MODEL ( Fernando work )
  */
-export enum ErrorType {
-  // eslint-disable-next-line no-unused-vars
-  UNKNOWN = 'UNKNOWN',
-  // eslint-disable-next-line no-unused-vars
-  EMPTY = 'EMPTY',
-  // eslint-disable-next-line no-unused-vars
-  NOSELECT = 'NOSELECT',
-  // eslint-disable-next-line no-unused-vars
-  NOSELECTIONS = 'NOSELECTIONS',
-  // eslint-disable-next-line no-unused-vars
-  NOFROM = 'NOFROM',
-  // eslint-disable-next-line no-unused-vars
-  INCOMPLETEFROM = 'INCOMPLETEFROM',
-  // eslint-disable-next-line no-unused-vars
-  INCOMPLETELIMIT = 'INCOMPLETELIMIT'
-}
+
+import { Soql } from '@salesforce/soql-model';
+
+// export enum ErrorType {
+//   // eslint-disable-next-line no-unused-vars
+//   UNKNOWN = 'UNKNOWN',
+//   // eslint-disable-next-line no-unused-vars
+//   EMPTY = 'EMPTY',
+//   // eslint-disable-next-line no-unused-vars
+//   NOSELECT = 'NOSELECT',
+//   // eslint-disable-next-line no-unused-vars
+//   NOSELECTIONS = 'NOSELECTIONS',
+//   // eslint-disable-next-line no-unused-vars
+//   NOFROM = 'NOFROM',
+//   // eslint-disable-next-line no-unused-vars
+//   INCOMPLETEFROM = 'INCOMPLETEFROM',
+//   // eslint-disable-next-line no-unused-vars
+//   INCOMPLETELIMIT = 'INCOMPLETELIMIT'
+// }
 
 // recoverable field errors
 export const recoverableFieldErrors = {};
-recoverableFieldErrors[ErrorType.NOSELECT] = true;
-recoverableFieldErrors[ErrorType.NOSELECTIONS] = true;
-recoverableFieldErrors[ErrorType.EMPTY] = true;
+recoverableFieldErrors[Soql.ErrorType.NOSELECT] = true;
+recoverableFieldErrors[Soql.ErrorType.NOSELECTIONS] = true;
+recoverableFieldErrors[Soql.ErrorType.EMPTY] = true;
 
 // recoverable from errors
 export const recoverableFromErrors = {};
-recoverableFromErrors[ErrorType.INCOMPLETEFROM] = true;
-recoverableFromErrors[ErrorType.NOFROM] = true;
-recoverableFromErrors[ErrorType.EMPTY] = true;
+recoverableFromErrors[Soql.ErrorType.INCOMPLETEFROM] = true;
+recoverableFromErrors[Soql.ErrorType.NOFROM] = true;
+recoverableFromErrors[Soql.ErrorType.EMPTY] = true;
 
 // recoverable limit errors
 export const recoverableLimitErrors = {};
-recoverableLimitErrors[ErrorType.INCOMPLETELIMIT] = true;
+recoverableLimitErrors[Soql.ErrorType.INCOMPLETELIMIT] = true;
+
+/**
+ * EMPTYWHERE = 'EMPTYWHERE',
+  INCOMPLETENESTEDCONDITION = 'INCOMPLETENESTEDCONDITION',
+  INCOMPLETEANDORCONDITION = 'INCOMPLETEANDORCONDITION',
+  INCOMPLETENOTCONDITION = 'INCOMPLETENOTCONDITION',
+  UNRECOGNIZEDCOMPAREVALUE = 'UNRECOGNIZEDCOMPAREVALUE',
+  UNRECOGNIZEDCOMPAREOPERATOR = 'UNRECOGNIZEDCOMPAREOPERATOR',
+  UNRECOGNIZEDCOMPAREFIELD = 'UNRECOGNIZEDCOMPAREFIELD',
+  NOCOMPAREVALUE = 'NOCOMPAREVALUE',
+  NOCOMPAREOPERATOR = 'NOCOMPAREOPERATOR'
+ */
+// recoverable where errors
+export const recoverableWhereErrors = {};
+recoverableWhereErrors[Soql.ErrorType.EMPTYWHERE] = true;
+recoverableWhereErrors[Soql.ErrorType.INCOMPLETENESTEDCONDITION] = true;
+recoverableWhereErrors[Soql.ErrorType.INCOMPLETEANDORCONDITION] = true;
+recoverableWhereErrors[Soql.ErrorType.INCOMPLETENOTCONDITION] = true;
+recoverableWhereErrors[Soql.ErrorType.UNRECOGNIZEDCOMPAREVALUE] = true;
+recoverableWhereErrors[Soql.ErrorType.UNRECOGNIZEDCOMPAREOPERATOR] = true;
+recoverableWhereErrors[Soql.ErrorType.UNRECOGNIZEDCOMPAREFIELD] = true;
+recoverableWhereErrors[Soql.ErrorType.NOCOMPAREVALUE] = true;
+recoverableWhereErrors[Soql.ErrorType.NOCOMPAREOPERATOR] = true;
 
 // general recoverable errors
 export const recoverableErrors = {
@@ -41,10 +67,10 @@ export const recoverableErrors = {
   ...recoverableFromErrors,
   ...recoverableLimitErrors
 };
-recoverableErrors[ErrorType.EMPTY] = true;
+recoverableErrors[Soql.ErrorType.EMPTY] = true;
 
 // unrecoverable errors
 export const unrecoverableErrors = {};
-unrecoverableErrors[ErrorType.UNKNOWN] = true;
+unrecoverableErrors[Soql.ErrorType.UNKNOWN] = true;
 
 // END ERROR HANDLING UTLIITIES
