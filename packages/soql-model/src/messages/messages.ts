e6878c54a0976d7c5b1955af129fe6fcdf38d69f
jgellin-sf <55159130+jgellin-sf@users.noreply.github.com>
2021-01-28 09:28:26 -0500

    Update packages/soql-model/src/messages/messages.ts
    Co-authored-by: Emily Kapner <ekapner@salesforce.com>

--- packages/soql-model/src/messages/messages.ts
+++ packages/soql-model/src/messages/messages.ts
@@ -21,7 +21,7 @@ export namespace Messages {
   export const error_unrecognizedCompareField = 'Unrecognized comparison field.';
   export const error_noCompareValue = 'Incomplete condition. Comparison value is required.';
   export const error_noCompareOperator = 'Incomplete condition. Comparison operator and value is required.';
-  export const error_incompleteMultiValueList = 'Incomplete values list. Values must be in parentheses and separated by commas.'
+  export const error_incompleteMultiValueList = 'Incomplete values list. Place values in parentheses, separated by commas.'
 
   export const error_fieldInput_boolean = 'Value must be TRUE or FALSE';
   export const error_fieldInput_currency = 'Currency value is not valid';
