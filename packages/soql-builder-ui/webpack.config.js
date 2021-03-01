70feba4b70034b288c0e5b6e98ae3f6fa1153180
Jonny Hork <jhork@salesforce.com>
2021-03-01 13:25:11 -0800

    Jhork/update lwc services (#206)
    *feat: update lwc-services to ^3.0.2 and support webpack 5

--- packages/soql-builder-ui/webpack.config.js
+++ packages/soql-builder-ui/webpack.config.js
@@ -1,8 +1,30 @@
-
-module.exports = 
-{
- node: {
-        fs: 'empty'
-      }
-}
-
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+module.exports = {
+  output: {
+    filename: 'app.js'
+  },
+  resolve: {
+    fallback: {
+      fs: false
+    },
+    alias: {
+      os: 'os-browserify/browser'
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      util: 'util/',
+      assert: 'assert/'
+    })
+  ],
+  optimization: {
+    // this removes license.txt from the output bundle
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false
+      })
+    ]
+  }
+};
